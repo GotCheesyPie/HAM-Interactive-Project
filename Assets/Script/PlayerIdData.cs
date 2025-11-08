3e2f11e5786e4bac55c2563ec2e4f98f1d0a147a
christodharma <christodharma@gmail.com>
2025-11-09 01:15:23 +0700

    basic player ID data

---
+++ Assets/Script/PlayerIdData.cs
@@ -0,0 +1,8 @@
+public class PlayerIdData
+{
+    public int PlayerId; // Primary Key
+    public string Name;
+    public int Age;
+    public string City;
+    public string ProfilePictureLink; // FIXME need integrating with storage
+}
